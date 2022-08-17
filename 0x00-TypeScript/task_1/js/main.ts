3569acaa8af5760bfe7327b6111b9ddd2c836b33
ilyes01 <ilyesghrab7@gmail.com>
2022-08-17 13:19:28 +0200

    students

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -19,3 +19,37 @@ interface printTeacherFunction {
 interface printTeacherFunction {
   (firstName: string, lastName: string): void;
 }
+
+
+
+
+
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface classConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
