7ed334cac2cac467169c7d354ede30f5affe6fc0
ilyes01 <ilyesghrab7@gmail.com>
2022-08-20 15:40:53 +0200

    taskkk nfiiikh

--- 0x02-react_intro/task_2/dashboard/src/App.js
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -11,26 +11,15 @@ function App() {
           <h1>School dashboard</h1>
         </div>
       </header>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br>
       <body className="App-body">
-        <h>Login to access the full dashboard</h>
-        <br></br>
+        <h>Login to access the full dashboard</h><br></br>
         <label for="email">Enter your email:</label>
         <input type="email" id="email" name="email"></input>
-        <br></br>
         <label for="pwd">Password:</label>
         <input type="password" id="pwd" name="pwd"></input>
-        <br></br>
+
         <button type="button">OK</button>
       </body>
       <footer className="App-footer">
