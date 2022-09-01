084655eac6159dfee716b5fd731f9d7d42290402
ilyes01 <ilyesghrab7@gmail.com>
2022-09-01 05:50:49 -0700

    vbzfken,

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+import $ from "jquery";
+import _ = require('lodash');
+
+$("<p>Holberton Dashboard</p>").appendTo($('body'));
+$("<p>Dashboard data for the students</p>").appendTo($('body'));
+$("<button>Click here to get started</button>").appendTo($('body'));
+$("<p id='count'></p>").appendTo($('body'));
+$("<p>Copyright - Holberton School</p>").appendTo($('body'));
+
+const updateCounter = _.debounce(() => {
+    let i= 0;
+    i+=1;
+    $("#count").text(`${i} clicks on the button`);
+});
+$('button').on('click', updateCounter);
