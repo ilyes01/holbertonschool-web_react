833845995f6ea91e418b679948b312eb9a4993b3
ilyes01 <ilyesghrab7@gmail.com>
2022-09-02 06:48:18 -0700

    update

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -3,10 +3,10 @@ const _ = require('lodash');
 
 $.when( $.ready ).then(function() {
 
-let i = 0;
+let counter = 0;
 function updateCounter() {
-    i+=1;
-    $('#count').html(`${i} clicks on the button`);
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
 };
 
 $('body').append('<p>Holberton Dashboard</p>');
@@ -15,5 +15,5 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append($('<p>').attr('id', 'count'));
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 300));
+$('button').on('click', _.debounce(updateCounter, 355));
 });
