2d72f56cec1c72fecc98f132659ecda914579f12
ilyes01 <ilyesghrab7@gmail.com>
2022-09-02 06:16:14 -0700

    cutvybkn,

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,12 +7,9 @@ $("<button>Click here to get started</button>").appendTo($('body'));
 $("<p id='count'></p>").appendTo($('body'));
 $("<p>Copyright - Holberton School</p>").appendTo($('body'));
 
-let i = 0;
-function updateCounter() {
-	i += 1;
-  $("#count").text(`${i} clicks on the button`);
-	return i;
-}
-$('button').on('click', _.debounce(() => {
-		let i = updateCounter();
-	}));
+const updateCounter = _.debounce(() => {
+    let i= 0;
+    i+=1;
+    $("#count").text(`${i} clicks on the button`);
+});
+$('button').on('click', updateCounter);
