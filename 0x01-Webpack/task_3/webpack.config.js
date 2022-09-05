5a33159bd677413ec047baabc4dec4cd9621cd05
ilyes01 <ilyesghrab7@gmail.com>
2022-09-05 12:21:56 -0700

    fixxxx

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     publicPath: '/public'
   },
   devServer: {
