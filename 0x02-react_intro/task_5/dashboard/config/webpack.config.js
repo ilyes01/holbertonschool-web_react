d33cc8f1f537f7ce3dc02d4bb262add1539e4ecd
ilyes01 <ilyesghrab7@gmail.com>
2022-09-08 20:40:09 +0200

    fiiixxx webpack.config

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -5,4 +5,26 @@ module.exports = {
 	path: `${__dirname}/dist`,
 	filename: 'bundle.js',
   },
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map"
+},
+
+rules: [{
+  test: /\.(gif|png|jpe?g|svg)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        bypassOnDebug: true, // webpack@1.x
+        disable: true,  // webpack@2.x and newer
+      },
+    },
+  ],
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+}]
 };
