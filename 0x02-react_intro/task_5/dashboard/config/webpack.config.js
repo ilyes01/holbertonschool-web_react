e32579aeca7a111bdbbd35d69720d448d0d521c6
ilyes01 <ilyesghrab7@gmail.com>
2022-09-08 23:57:07 +0200

    update

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,30 +1,45 @@
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-	path: `${__dirname}/dist`,
-	filename: 'bundle.js',
-  },
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map"
-},
+const path = require('path');
 
-rules: [{
-  test: /\.(gif|png|jpe?g|svg)$/i,
-  use: [
-    'file-loader',
-    {
-      loader: 'image-webpack-loader',
-      options: {
-        bypassOnDebug: true, // webpack@1.x
-        disable: true,  // webpack@2.x and newer
+module.exports = {
+  mode: "development", // "production" | "development" | "none"
+  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  entry: "./src/index.js", // string | object | array
+  // defaults to ./src
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+    // options related to how webpack emits results
+    path:path.resolve(__dirname, "dist"), // string (default)
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "bundle.js", // string (default)
+    uniqueName: "my-application", // (defaults to package.json "name")
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+        // Conditions:
+       {
+        test: /\.css/$i,
+
+
+        use: [
+          // apply multiple loaders and options instead
+          "style-loader",
+          "css-loader",
+              // ...
+        ],
       },
-    },
-  ],
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-}]
+  {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+    loader: 'image-webpack-loader',
+  },
+],
+devtool: "inline-map", // enum
+// enhance debugging by adding meta info for the browser devtools
+// source-map most detailed at the expense of build speed.
+  }
 };
+
