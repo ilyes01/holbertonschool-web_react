fb0ca37550eda9b401b1eed1ab1bad1fc778202a
ilyes01 <ilyesghrab7@gmail.com>
2022-09-09 23:45:04 +0200

    updaaaate

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development", // "production" | "development" | "none"
-  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+
   entry: "./src/index.js", // string | object | array
   // defaults to ./src
   // Here the application starts executing
@@ -13,38 +14,82 @@ module.exports = {
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
     filename: "bundle.js", // string (default)
-    uniqueName: "my-application", // (defaults to package.json "name")
+    // the filename template for entry chunks
   },
   module: {
     // configuration regarding modules
     rules: [
       // rules for modules (configure loaders, parser options, etc.)
+      {
         // Conditions:
-       {
-        test: /\.css/$i,
-
-
+        test: /\.css$/i,
+       
+        // these are matching conditions, each accepting a regular expression or string
+        // test and include have the same behavior, both must be matched
+       
         use: [
           // apply multiple loaders and options instead
           "style-loader",
           "css-loader",
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+              // specifies the module type
+        /* Advanced actions (click to show) */
+            loader: 'image-webpack-loader',
+          
+            options: {
               // ...
-        ],
-      },
-  {
-    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    type: 'asset/resource',
-    loader: 'image-webpack-loader',
-  },
-      {
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-			}
-],
-devtool: "inline-map", // enum
-// enhance debugging by adding meta info for the browser devtools
-// source-map most detailed at the expense of build speed.
-  }
-};
+            }
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+          }
+          ],
 
+      
+        // specifies the module type
+        /* Advanced actions (click to show) */
+      },
+  
+      {
+        // ... (conditions)
+        rules: [
+          // ... (rules)
+        ]
+        // use all of these nested rules (combine with conditions to be useful)
+      },
+    ],
+    /* Advanced module configuration (click to show) */
+  },
+
+
+  devtool: "inline-source-map", // enum
+  // enhance debugging by adding meta info for the browser devtools
+  // source-map most detailed at the expense of build speed.
+ 
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8564,
+    hot: true
+  
+ 
+  },
+  plugins: [
+    // ...
+    new HtmlWebpackPlugin({
+			template: './dist/index.html',
+      title: 'Holberton Dashboard',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  // list of additional plugins
+  mode: 'development'
+
+  
+  };
+  /* Advanced configuration (click to show) */
+  /* Advanced caching configuration (click to show) */
+  /* Advanced build configuration (click to show) */
